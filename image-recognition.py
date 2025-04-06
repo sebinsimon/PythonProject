e8115d41a3a2a9ac72db49a50261e6a15a54fc71
Sebin Simon <sebinsimon70@gmail.com>
2025-04-06 13:13:46 +0100

    image processing

--- image-recognition.py
+++ image-recognition.py
@@ -6,3 +6,8 @@ import matplotlib.pyplot as plt
 # ML/DL
 import tensorflow as tf
 from tensorflow import keras
+
+# Image processing
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+from keras.models import Sequential
